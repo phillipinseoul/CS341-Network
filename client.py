c649269f2b8ec9f93dc4d71b1eab5d088171b8db
phillip <phillip9807@gmail.com>
2020-09-14 23:37:28 +0900

    add collatz post method - no repeat

--- client.py
+++ client.py
@@ -1,5 +1,22 @@
 import sys
+import requests, json
 
 url = sys.argv[1]
 name = sys.argv[2]
 number = int(sys.argv[3])
+
+pg = requests.get(url + "/hello_world").json()
+print(pg["message"])
+
+params = {'name': name}
+
+hash = requests.get(url + "/hash", params=params).json()
+hash_res = hash["result"]
+print(hash_res)
+
+data = {"name": name, "hash": hash_res, "number": number}
+
+collatz = requests.post(url + "/collatz", data=data).json()
+
+print(collatz['result'])
+
--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import json, hashlib
 
 app = Flask(__name__)
@@ -11,12 +11,20 @@ def route_hello_world():
 def route_hash():
     name = request.args.get('name')
     HASHED_VALUE = hashlib.sha256(name.encode()).hexdigest()
-    return json.dumps({'result': f'{HASHED_VALUE}'})
+    return json.dumps({'result': HASHED_VALUE})
 
 @app.route('/collatz', methods=['POST'])
 def route_collatz():
-    return json.dumps({'result': 'NEXT_COLLATZ_NUMBER'})
+    # recv = request.get_json()
 
+    number = request.form.get("number")
+    
+    if number % 2 == 1:
+        NEXT_COLLATZ_NUMBER = 3 * number + 1
+    else:
+        NEXT_COLLATZ_NUMBER = number / 2
+
+    return {'result': NEXT_COLLATZ_NUMBER}
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
