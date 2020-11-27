66f9c720d6871dfa99ade3db2b396fc0f2358da2
PHILLIP LEE <phillip9807@gmail.com>
2020-11-27 17:24:34 +0900

    rename

---
+++ task2-2-2.cc
@@ -0,0 +1,208 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/*
+ * Copyright (c) 2009 IITP RAS
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *
+ * Authors: Pavel Boyko <boyko@iitp.ru>
+ */
+
+#include <iostream>
+#include <cmath>
+#include "ns3/aodv-module.h"
+#include "ns3/core-module.h"
+#include "ns3/network-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/mobility-module.h"
+#include "ns3/point-to-point-module.h"
+#include "ns3/v4ping-helper.h"
+#include "ns3/yans-wifi-helper.h"
+
+using namespace ns3;
+
+class Task2 
+{
+public:
+  Task2 ();
+  /// Run simulation
+  void Run ();
+
+private:
+
+  // parameters
+  /// Number of nodes
+  uint32_t size;
+  /// Width between nodes, meters
+  double width;
+  /// Height between nodes, meters
+  double height;
+  /// Simulation time, seconds
+  double totalTime;
+  /// Print routes if true
+  bool printRoutes;
+
+  // network
+  /// nodes used in the example
+  NodeContainer nodes;
+  /// devices used in the example
+  NetDeviceContainer devices;
+  /// interfaces used in the example
+  Ipv4InterfaceContainer interfaces;
+
+private:
+  /// Create the nodes
+  void CreateNodes ();
+  /// Create the devices
+  void CreateDevices ();
+  /// Create the network
+  void InstallInternetStack ();
+  /// Create the simulation applications
+  void InstallApplications ();
+};
+
+int main (int argc, char **argv)
+{
+  Task2 test;
+
+  test.Run ();
+  return 0;
+}
+
+//-----------------------------------------------------------------------------
+Task2::Task2 () :
+  size (10),
+  width (50),
+  height (0),
+  totalTime (100),
+  printRoutes (true)
+{
+}
+
+
+void
+Task2::Run ()
+{
+//  Config::SetDefault ("ns3::WifiRemoteStationManager::RtsCtsThreshold", UintegerValue (1)); // enable rts cts all the time.
+  CreateNodes ();
+  CreateDevices ();
+  InstallInternetStack ();
+  InstallApplications ();
+
+  std::cout << "Starting simulation for " << totalTime << " s ...\n";
+
+  Simulator::Stop (Seconds (totalTime));
+  Simulator::Run ();
+
+  // DELETE THIS CODE!
+  for (NodeContainer::Iterator j = nodes.Begin(); j != nodes.End(); ++j)
+      {
+        Ptr<Node> object = *j;
+        Ptr<MobilityModel> position = object->GetObject<MobilityModel>();
+        NS_ASSERT(position != 0);
+        Vector pos = position->GetPosition();
+        std::cout << "x=" << pos.x << ", y=" << pos.y << ", z=" << pos.z << std::endl;
+      }
+
+  Simulator::Destroy ();
+}
+
+
+void
+Task2::CreateNodes ()
+{
+  std::cout << "Creating " << (unsigned)size << " nodes " << width << " m apart.\n";
+  nodes.Create (size);
+  // Name nodes
+  for (uint32_t i = 0; i < size; ++i)
+    {
+      std::ostringstream os;
+      os << "node-" << i;
+      Names::Add (os.str (), nodes.Get (i));
+    }
+  // Create static grid
+  MobilityHelper mobility;
+  mobility.SetPositionAllocator ("ns3::GridPositionAllocator",
+                                 "MinX", DoubleValue (0.0),
+                                 "MinY", DoubleValue (0.0),
+                                 "DeltaX", DoubleValue (width),
+                                 "DeltaY", DoubleValue (height),
+                                 "GridWidth", UintegerValue (size),
+                                 "LayoutType", StringValue ("RowFirst"));
+  mobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
+  mobility.Install (nodes);
+
+  // DELETE THIS CODE!
+  // iterate our nodes and print their position
+  for (NodeContainer::Iterator j = nodes.Begin(); j != nodes.End(); ++j)
+      {
+        Ptr<Node> object = *j;
+        Ptr<MobilityModel> position = object->GetObject<MobilityModel>();
+        NS_ASSERT(position != 0);
+        Vector pos = position->GetPosition();
+        std::cout << "x=" << pos.x << ", y=" << pos.y << ", z=" << pos.z << std::endl;
+      }
+  
+}
+
+void
+Task2::CreateDevices ()
+{
+  WifiMacHelper wifiMac;
+  wifiMac.SetType ("ns3::AdhocWifiMac");
+  YansWifiPhyHelper wifiPhy = YansWifiPhyHelper::Default ();
+  YansWifiChannelHelper wifiChannel = YansWifiChannelHelper::Default ();
+  wifiPhy.SetChannel (wifiChannel.Create ());
+  WifiHelper wifi;
+  wifi.SetRemoteStationManager ("ns3::ConstantRateWifiManager", "DataMode", StringValue ("OfdmRate6Mbps"), "RtsCtsThreshold", UintegerValue (0));
+  devices = wifi.Install (wifiPhy, wifiMac, nodes); 
+}
+
+void
+Task2::InstallInternetStack ()
+{
+  AodvHelper aodv;
+  // you can configure AODV attributes here using aodv.Set(name, value)
+  InternetStackHelper stack;
+  stack.SetRoutingHelper (aodv); // has effect on the next Install ()
+  stack.Install (nodes);
+  Ipv4AddressHelper address;
+  address.SetBase ("10.0.0.0", "255.0.0.0");
+  interfaces = address.Assign (devices);
+
+  if (printRoutes)
+    {
+      Ptr<OutputStreamWrapper> routingStream = Create<OutputStreamWrapper> ("scratch/task2.routes", std::ios::out);
+      aodv.PrintRoutingTableAllAt (Seconds (totalTime / 2), routingStream);
+    }
+}
+
+void
+Task2::InstallApplications ()
+{
+  // Ping from Node J to Node C
+  V4PingHelper ping (interfaces.GetAddress (9));  // 9 -> Node J
+  ping.SetAttribute ("Verbose", BooleanValue (true));
+
+  ApplicationContainer p = ping.Install (nodes.Get (2));  // 2 -> Node C
+  p.Start (Seconds (0));
+  p.Stop (Seconds (totalTime) - Seconds (0.001));
+
+  // Move selected node away
+  Ptr<Node> node = nodes.Get (7);   // Get Node H
+  Ptr<MobilityModel> mob = node->GetObject<MobilityModel> ();
+  
+  // Modify this line to satisfy the movement condition from the task 
+  Simulator::Schedule (Seconds (totalTime / 3), &MobilityModel::SetPosition, mob, Vector (75, 0, 0));   // Move Node H to between B and C
+}
